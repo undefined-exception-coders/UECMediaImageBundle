bc0a272362fb7d279aa76e96488df07324c85556
Damiano Ciarla <damiano.ciarla@gmail.com>
2016-02-28 09:15:46 +0100

    OptionsResolverInterface in OptionsResolver

--- Form/Type/MediaImageFormType.php
+++ Form/Type/MediaImageFormType.php
@@ -4,7 +4,7 @@ namespace UEC\MediaImageBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MediaImageFormType extends AbstractType
 {
@@ -25,7 +25,7 @@ class MediaImageFormType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => $this->modelClass,
